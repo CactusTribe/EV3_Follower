4c2be0fa4cf7a97387ce0708eb78f310372fbfdb
CactusTribe <lefrancjoaquim@gmail.com>
2016-09-24 19:28:48 +0200

    Ajout de la lib c++ et test concluant avec le moteur

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "../header/ev3dev.h"
 
 using namespace std;
+using namespace ev3dev;
+
 
 int main(){
-	cout << "Hello world !" << endl;
+	cout << "Start engine !" << endl;
+
+	int speed = 500; 
+	int time = 2000; 								// ms
+	large_motor  motor_A(OUTPUT_A); // PORT (A|B|C|D)
+	
+	motor_A.set_speed_sp(speed);
+	motor_A.set_time_sp(time).run_timed();
+	
 }
