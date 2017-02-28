40f8fdf76a5340791e1de8ed0f1ac345003e7d42
SKODA jerome <skoda@informatique.univ-paris-diderot.fr>
2017-02-28 16:27:48 +0100

    micro Refactoring

--- cpp/src/Devices/Engine.cpp
+++ cpp/src/Devices/Engine.cpp
@@ -4,80 +4,88 @@ using namespace ev3dev;
 
 Engine::Engine()
 {
-	_motor_R = new large_motor(OUTPUT_A);
-	_motor_L = new large_motor(OUTPUT_D);
+  _motor_R = new large_motor(OUTPUT_A);
+  _motor_L = new large_motor(OUTPUT_D);
 
-	_direction = Direction::FORWARD;
-	_run = false;
-	
-	_motor_L->run_direct();
-	_motor_R->run_direct();
+  _direction = Direction::FORWARD;
+  _run = false;
+
+  _motor_L->run_direct();
+  _motor_R->run_direct();
 }
 
 Engine::~Engine()
 {
-	_motor_R->reset();
-	_motor_L->reset();
+  _motor_R->reset();
+  _motor_L->reset();
 
-	delete _motor_R;
-	delete _motor_L;
+  delete _motor_R;
+  delete _motor_L;
 }
+
+void Engine::_updateDutyCycle()
+{
+  _motor_L->set_duty_cycle_sp(_speed * _ratio.l);
+  _motor_R->set_duty_cycle_sp(_speed * _ratio.r);
+}
+
 
 
 void Engine::setSpeed(int speed)
 {
-	_speed = speed;
-	_motor_L->set_duty_cycle_sp(_speed);
-	_motor_R->set_duty_cycle_sp(_speed);
+  _speed = speed;
+  _updateDutyCycle();
 }
 
 void Engine::setDirection(Direction dir)
 {
-	_direction = dir;
+  _direction = dir;
 }
+
+void Engine::setRatio(DutyRatio ratio)
+{
+  _ratio= ratio;
+  _updateDutyCycle();
+}
+
 
 
 void Engine::run()
 {
 
-	switch(_direction){
-		case Direction::FORWARD:
-			_motor_L->set_duty_cycle_sp(_speed);
-			_motor_R->set_duty_cycle_sp(_speed);
-		break;
-		case Direction::BACKWARD:
-			_motor_L->set_duty_cycle_sp(_speed * -1);
-			_motor_R->set_duty_cycle_sp(_speed * -1);
-		break;
-		case Direction::LEFT:
-			_motor_L->set_duty_cycle_sp(_speed * -0.5);
-			_motor_R->set_duty_cycle_sp(_speed);
-		break;
-		case Direction::RIGHT:
-			_motor_L->set_duty_cycle_sp(_speed);
-			_motor_R->set_duty_cycle_sp(_speed * -0.5);
-		break;
-		default:
-			_motor_L->set_duty_cycle_sp(0);
-			_motor_R->set_duty_cycle_sp(0);
-		break;
-	}
+  switch(_direction){
+    case Direction::FORWARD:
+      setRatio(DutyCycleRatio::FORWARD);
+    break;
+    case Direction::BACKWARD:
+      setRatio(DutyCycleRatio::BACKWARD);
+    break;
+    case Direction::LEFT:
+      setRatio(DutyCycleRatio::LEFT);
+    break;
+    case Direction::RIGHT:
+      setRatio(DutyCycleRatio::RIGHT);
+    break;
+    default:
+      setRatio(DutyCycleRatio::STOP);
+    break;
+  }
 
-	//_motor_L->run_direct();
-	//_motor_R->run_direct();
-	_run = true;
+  //_motor_L->run_direct();
+  //_motor_R->run_direct();
+  _run = true;
 }
 
 void Engine::stop()
 {
-	//_motor_L->stop();
-	//_motor_R->stop();
-	_motor_L->set_duty_cycle_sp(0);
-	_motor_R->set_duty_cycle_sp(0);
-	_run = false;
+  //_motor_L->stop();
+  //_motor_R->stop();
+  _motor_L->set_duty_cycle_sp(0);
+  _motor_R->set_duty_cycle_sp(0);
+  _run = false;
 }
 
 bool Engine::running()
 {
-	return _run;
+  return _run;
 }
--- cpp/src/Devices/Engine.h
+++ cpp/src/Devices/Engine.h
@@ -3,26 +3,29 @@
 
 #include "../libs/ev3dev.h"
 #include "../commons/Direction.h"
+#include "../commons/DutyRatio.h"
 
 class Engine {
 
 public:
-	Engine();
-	virtual ~Engine();
+  Engine();
+  virtual ~Engine();
 
-	void setSpeed(int speed);
-	void setDirection(Direction dir);
-
-	void run();
-	void stop();
-	bool running();
+  void setSpeed(int speed);
+  void setDirection(Direction dir);
+  void setRatio(DutyRatio ratio);
+  void run();
+  void stop();
+  bool running();
 
 private:
-	int _speed = 30;
-	bool _run = false;
-	Direction _direction; 
-	ev3dev::large_motor* _motor_R;
-	ev3dev::large_motor* _motor_L;
+  void _updateDutyCycle();
+  int _speed = 30;
+  bool _run = false;
+  Direction _direction;
+  DutyRatio _ratio= DutyCycleRatio::STOP;
+  ev3dev::large_motor* _motor_R;
+  ev3dev::large_motor* _motor_L;
 
 };
 
